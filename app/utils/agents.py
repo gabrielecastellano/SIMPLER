07e18f6ea2fe4601dc54f80fb7f67642e784baaa
gabrielecastellano <gabry.c92x@gmail.com>
2023-12-13 21:27:29 +0100

    minor

--- app/utils/agents.py
+++ app/utils/agents.py
@@ -1,13 +1,13 @@
 import sys
 import numpy as np
 
-np.set_printoptions(threshold=sys.maxsize)
 import random
 import string
 
-import config
-
 from stable_baselines import logger
+
+
+np.set_printoptions(threshold=sys.maxsize)
 
 
 def sample_action(action_probs):
@@ -21,25 +21,25 @@ def mask_actions(legal_actions, action_probs):
     return masked_action_probs
 
 
-class Agent():
+class Agent:
     def __init__(self, name, model=None):
         self.name = name
-        self.id = self.name + '_' + ''.join(random.choice(string.ascii_lowercase) for x in range(5))
+        self.id = self.name + '_' + ''.join(random.choice(string.ascii_lowercase) for _ in range(5))
         self.model = model
         self.points = 0
 
-    def print_top_actions(self, action_probs):
+    @staticmethod
+    def print_top_actions(action_probs):
         top5_action_idx = np.argsort(-action_probs)[:5]
         top5_actions = action_probs[top5_action_idx]
         print(top5_actions)
         print(top5_action_idx)
-        logger.debug(
-            f"Top 5 actions: {[str(i) + ': ' + str(round(a, 2))[:5] for i, a in zip(top5_action_idx, top5_actions)]}")
+        logger.debug(f"Top 5 actions: "
+                     f"{[str(i) + ': ' + str(round(a, 2))[:5] for i, a in zip(top5_action_idx, top5_actions)]}")
 
     def choose_action(self, env, choose_best_action, mask_invalid_actions):
         if self.name == 'rules':
             action_probs = np.array(env.rules_move())
-            value = None
         else:
             action_probs = self.model.action_probability(env.observation)
             value = self.model.policy_pi.value(np.array([env.observation]))[0]
